523b29dc0e7770b40add0daaef5e5952bff43d3d
mostafa-razavi <sr87@zips.uakron.edu>
2020-04-15 17:58:17 -0400

    Fixed a bug in RunITIC_MultipleMolecules_OneFF.sh

--- Scripts/RunITIC_MultipleMolecules_OneFF.sh
+++ Scripts/RunITIC_MultipleMolecules_OneFF.sh
@@ -47,7 +47,7 @@ do
     MW=$(grep "MW:" ${ITIC_file_name} | awk '{ print $2}')
 
        # Run excl 
-    bash $HOME/Git/ITIC_GROMACS/Scripts/RerunITIC_GROMACS_Parallel.sh $molec 50 32 #bash $HOME/Git/ITIC_GROMACS/Scripts/RunExcl_GROMACS_Parallel.sh $molec 50 32 500000
+    bash $HOME/Git/ITIC_GROMACS/Scripts/RerunITIC_GROMACS_Parallel.sh $molec 50 $Nproc #bash $HOME/Git/ITIC_GROMACS/Scripts/RunExcl_GROMACS_Parallel.sh $molec 50 $Nproc 500000
 
        # Get averages
     rm -rf EnergyOut/ *.res
